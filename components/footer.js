cf8afb622fa4311215af8d3d87915ed187c30824
Tyler Nix <tylernix@c02dc8t2ml7l.attlocal.net>
2020-12-04 16:39:58 -0600

    adding base_url environment variable

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import Container from './container'
-import { BASE_URL, GITHUB_URL } from '../lib/constants'
+import { GITHUB_URL } from '../lib/constants'
 
 export default function Footer() {
   return (
@@ -11,7 +11,7 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href={`${BASE_URL}/posts/about`}
+              href={`${process.env.BASE_URL}/posts/about`}
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Read about me
