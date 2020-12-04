edd43bc1217d42a8931735ba064712fa6738da72
Tyler Nix <tylernix@c02dc8t2ml7l.attlocal.net>
2020-12-04 15:57:21 -0600

    updating README

--- components/intro.js
+++ components/intro.js
@@ -3,18 +3,19 @@ import { CMS_NAME } from '../lib/constants'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      {/* <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+      </h1> */}
+      <img src="assets/tnix.png"></img>
+      <h4 className="text-center md:text-right text-lg mt-5 md:pl-8">
+        I am a Senior Solutions Engineer at {' '}
         <a
-          href="https://nextjs.org/"
+          href="https://auth0.com"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          Next.js
+          Auth0
         </a>{' '}
-        and {CMS_NAME}.
+        striving to make the internet a more secure place, <b>one login at a time</b>.
       </h4>
     </section>
   )
