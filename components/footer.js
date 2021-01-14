dde0155eece05117fb18c9888375aba55aa5132c
Tyler Nix <tylernix@pm.me>
2021-01-14 17:28:19 -0600

    footer links target fix

--- components/footer.js
+++ components/footer.js
@@ -12,17 +12,17 @@ export default function Footer() {
           <div className="flex flex-col space-y-2.5 text-left border-l-2 border-prussian-blue pl-4">
             <p>
               Designed using 
-                <a href="https://figma.com" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2">figma</a>
+                <a href="https://figma.com" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2" target="_blank">figma</a>
             </p>
             <p>
               Built using 
-                <a href="https://reactjs.org" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2">react</a> 
-                <a href="https://nextjs.org" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2">next.js</a> 
-                <a href="https://tailwindcss.com" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2">tailwindcss</a>
+                <a href="https://reactjs.org" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2" target="_blank">react</a> 
+                <a href="https://nextjs.org" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2" target="_blank">next.js</a> 
+                <a href="https://tailwindcss.com" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2" target="_blank">tailwindcss</a>
             </p>
             <p>
               Deployed using 
-                <a href="https://vercel.com" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2">vercel</a>
+                <a href="https://vercel.com" className="py-1 px-2 rounded-full bg-imperial-red text-white text-sm ml-2" target="_blank">vercel</a>
             </p> 
           </div>
           
