d137e56a9ffed38bfe96786c80a96ea73eeee304
Tyler Nix <tylernix@pm.me>
2021-01-21 16:11:12 -0600

    adding Auth greeting based on if the user is signed in or not

--- components/header.js
+++ components/header.js
@@ -1,8 +1,23 @@
 import Link from "next/link";
 import Container from "../components/container";
 import { GITHUB_URL, LINKEDIN_URL } from "../lib/constants";
+import { useUser } from '@auth0/nextjs-auth0';
+
 
 export default function Header() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  let greeting;
+  if (user) greeting = <a  href="/api/auth/logout" className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
+                            Log out
+                          </a>
+  else greeting = <a  href="/api/auth/login" className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
+                      Sign In
+                    </a>
+
   return (
     <Container>
       <div class="flex justify-between text-sm text-prussian-blue flex-col md:flex-row flex items-left md:items-center md:justify-between my-8 md:my-12">
@@ -19,12 +34,7 @@ export default function Header() {
           <a href="/blog" class="mx-3 text-base hover:underline">
             Blog
           </a>
-          <a
-            href="/api/auth/login"
-            className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-          >
-            Sign In
-          </a>
+          {greeting}
         </div>
       </div>
     </Container>
--- pages/profile.js
+++ pages/profile.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import WebAuthn from '../components/webauthn'
 import Layout from '../components/layout'
 import Header from '../components/header'
-import Container from '../components/container'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
 
 export default function Profile() {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  
+
   return (
-    user && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
-  );
+    <>
+      <Layout>
+        <Head>
+          <title>Profile</title>
+        </Head>
+          <Header />
+          <Container>
+              {user && 
+                <div>
+                  <img src={user.picture} alt={user.name} />
+                  <h2>{user.name}</h2>
+                  <p>{user.email}</p>
+                </div>
+              }
+              {!user && 
+                <div>
+                  <p>Please sign in to see your profile.</p>
+                </div>
+              }        
+          </Container>
+      </Layout>
+    </>
+  )
 }
