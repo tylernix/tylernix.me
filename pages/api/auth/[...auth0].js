87aa19ad645111c84e1e48e0b1fcd73ef7f20c42
Tyler Nix <tylernix@pm.me>
2021-03-15 11:15:08 -0500

    removing comments

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,21 +1,4 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-//import { loginHandler } from '@auth0/nextjs-auth0/dist/auth0-session';
-
-// const getLoginState = (req, loginOptions) => {
-//     return { basket_id: getBasketId(req) };
-//   };
-
-// const loginOptions = {
-//     authorizationParams: [{
-//         response_type: 'code',
-//         scope: 'openid profile email',
-
-//         // Additional parameters
-//         acr_value: "tenant:test-tenant",
-//         custom_param: "custom-value"
-//     }],
-//     getLoginState
-// }
 
 let returnTo = "/profile";
 
