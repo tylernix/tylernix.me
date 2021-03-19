04964996965ceca19888e0d7c0a7355c7e4cf2ad
Tyler Nix <tylernix@pm.me>
2021-03-19 13:35:54 -0500

    adding js branding folder

---
+++ public/assets/branding/js/iiLFKyZ9J2Yf0bPJykarAaX716umkDJ9.js
@@ -0,0 +1,2 @@
+/* JS for Mars App (client_id: iiLFKyZ9J2Yf0bPJykarAaX716umkDJ9) */
+document.querySelector(h1).text="Welcome to Mars"
---
+++ public/assets/branding/js/t063byHVfxbdfV7HKiU0Jq9EPzmgkB1I.js
@@ -0,0 +1,2 @@
+/* JS for Moon App (client_id: t063byHVfxbdfV7HKiU0Jq9EPzmgkB1I) */
+document.querySelector(h1).text="Welcome to the Moon"
