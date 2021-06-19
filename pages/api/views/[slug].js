d17eb350542d2e361594dc94a038cdb870a9cdd4
Tyler Nix <tnix43@gmail.com>
2021-06-19 12:27:41 -0600

    removing do not count page views on localhost

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -3,8 +3,8 @@ import db from '../../../lib/firebase';
 export default async (req, res) => {
   if (req.method === 'POST') {
     // do not count page views on localhost
-    if(req.hostname.contains("localhost"))
-      return res.status(200);
+    // if(req.hostname.contains("localhost"))
+    //   return res.status(200);
 
     const ref = db.ref('views').child(req.query.slug);
     const { snapshot } = await ref.transaction((currentViews) => {
