e056acc9a37b31b06098a72ef32d8599851b94be
Tyler Nix <tnix43@gmail.com>
2021-06-19 12:13:50 -0600

    do not count page views on localhost

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -2,6 +2,10 @@ import db from '../../../lib/firebase';
 
 export default async (req, res) => {
   if (req.method === 'POST') {
+    // do not count page views on localhost
+    if(req.hostname.contains("localhost"))
+      return res.status(200);
+
     const ref = db.ref('views').child(req.query.slug);
     const { snapshot } = await ref.transaction((currentViews) => {
       if (currentViews === null) {
