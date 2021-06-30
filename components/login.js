2787c6f42c132f5d35911c8ee657df4f115f1230
Tyler Nix <tylernix@pm.me>
2021-06-30 09:41:00 -0500

    working implementation of passwordless auth with magic-link

--- components/login.js
+++ components/login.js
@@ -1,37 +1,47 @@
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
+import Link from 'next/link'
+import { useState } from 'react'
+import useUser from '../lib/hooks'
+
 import { Magic } from 'magic-sdk'
 
 export default function Login() {
-  const router = useRouter()
-  const handleSubmit = async (event) => {
-    event.preventDefault()
+    const { user, error } = useUser();
+    const router = useRouter()
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+    
+      const { elements } = event.target
+    
+      // the Magic code
+      const did = await new Magic(process.env.NEXT_PUBLIC_MAGIC_PUB_KEY)
+        .auth
+        .loginWithMagicLink({ email: elements.email.value })
+    
+        const authRequest = await fetch('/api/login', {
+            method: 'POST',
+            headers: { Authorization: `Bearer ${did}` }
+          })
+        
+          if (authRequest.ok) {
+            // We successfully logged in, our API
+            // set authorization cookies and now we
+            // can redirect to the dashboard!
+            router.push('/profile')
+          } else { /* handle errors */ }
+    }
   
-    const { elements } = event.target
-  
-    // the Magic code
-    const did = await new Magic(process.env.NEXT_PUBLIC_MAGIC_PUB_KEY)
-      .auth
-      .loginWithMagicLink({ email: elements.email.value })
-  
-     // Once we have the did from magic, login with our own API
-    const authRequest = await fetch('/api/login', {
-        method: 'POST',
-        headers: { Authorization: `Bearer ${did}` }
-    })
-
-    if (authRequest.ok) {
-        // We successfully logged in, our API
-        // set authorization cookies and now we
-        // can redirect to the dashboard!
-        router.push('/profile')
-    } else { /* handle errors */ }
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='email'>Email</label>
-      <input name='email' type='email' />
-      <button>Log in</button>
-    </form>
-  )
+    return (
+        <>
+            { !user && (
+                <form onSubmit={handleSubmit}>
+                    <input name='email' type='email' placeholder="your@email.com" required />
+                    <button type="submit">Log in</button>
+                </form>
+            )}
+            {user && (
+                <a href="/api/logout">Log Out</a>
+            )} 
+        </>
+    )
 }
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,21 +1,16 @@
-import axios from 'axios'
-import Iron from '@hapi/iron'
-import CookieService from '../../lib/cookie'
-import { Magic } from '@magic-sdk/admin'
+import { magic } from '../../lib/magic'
+import { setLoginSession } from '../../lib/auth'
 
-let magic = new Magic(process.env.MAGIC_SECRET_KEY)
+export default async function login(req, res) {
+  try {
+    const did = req.headers.authorization.split('Bearer').pop().trim();
+    const metadata = await magic.users.getMetadataByToken(did);
+    const session = { ...metadata }
 
-export default async (req, res) => {
-    if (req.method !== 'POST') return res.status(405).end()
+    await setLoginSession(res, session)
 
-    // exchange the did from Magic for some user data
-    const did = req.headers.authorization.split('Bearer').pop().trim()
-    console.log(magic.token.getPublicAddress(did));
-    const user = await new Magic(process.env.MAGIC_SECRET_KEY).users.getMetadataByToken(did)
-
-    // Author a couple of cookies to persist a user's session
-    const token = await Iron.seal(user, process.env.ENCRYPTION_SECRET, Iron.defaults)
-    CookieService.setTokenCookie(res, token)
-
-    res.end()
+    return res.status(200).send({ done: true })
+  } catch (error) {
+    return res.status(error.status || 500).end(error.message)
+  }
 }
