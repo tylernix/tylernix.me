3e4c5be863760e5b8a0fa34574c7ea432bdc895f
Tyler Nix <67964959+tylernix@users.noreply.github.com>
2023-12-28 11:09:43 -0600

    Update date-formatter.js to remove 'about'

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -3,7 +3,7 @@ import { parseISO, format, formatDistanceToNow } from 'date-fns';
 export default function DateFormatter({ dateString, displayDistance }) {
   const date = parseISO(dateString)
   if (displayDistance) {
-    return <time dateTime={dateString}>{format(date, 'EEEE, MMMM	d, yyyy')} (about {formatDistanceToNow(date)} ago)</time>
+    return <time dateTime={dateString}>{format(date, 'EEEE, MMMM	d, yyyy')} ({formatDistanceToNow(date)} ago)</time>
   } else {
     return <time dateTime={dateString}>{format(date, 'EEEE, MMMM	d, yyyy')}</time>
   }
